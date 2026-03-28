c5210780b7395166302d7c04d7ff80f073bd7f91
DylanPerkins <dp011400@gmail.com>
2026-03-28 19:06:39 -0400

    Remove redundant @Column imports from entity classes

--- anime-reviews/src/main/java/anime_reviews/entity/Users.java
+++ anime-reviews/src/main/java/anime_reviews/entity/Users.java
@@ -4,7 +4,6 @@ import java.util.HashSet;
 import java.util.Set;
 
 import jakarta.persistence.CascadeType;
-import jakarta.persistence.Column;
 import jakarta.persistence.ElementCollection;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
